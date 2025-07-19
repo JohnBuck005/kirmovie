68d9df345b2680ee389d2e81cf5195cad9e763c4
JohnBuckz <buckridgejohnathan30@gmail.com>
2025-07-19 07:29:37 +0300

    ❤️ Add FavoritesScreen placeholder

---
+++ src/screens/FavoritesScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function FavoritesScreen() {
+  return (
+    <SafeAreaView className="flex-1 bg-slate-900">
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-white text-xl">Favorites Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
